adb38156141d8c42f9bd0bb2b639baa98c8cf60a
writingskillfully <anokaze@gmail.com>
2014-01-10 23:31:46 -0500

    hardcoded solution to Dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,10 +1,11 @@
 d={}
 
 
-sent="We tried list and we tried dicts also we tried Zen"
+sent="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be"
 for word in sent.split(' '):
 	if word in d:
 		d[word]=d[word]+1
 	else:
 		d[word]=1
-print d
+for key, value in d.items():
+    print key, " " , value
