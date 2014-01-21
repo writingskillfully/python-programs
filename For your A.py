25700876f797030670838602fda9690c455fbf6e
writingskillfully <anokaze@gmail.com>
2014-01-21 15:32:26 -0500

    A solution to Code Academy's loops module

---
+++ For your A.py
@@ -0,0 +1,13 @@
+s = "A bird in the hand..."
+
+# Add your for loop
+for c in s:
+    if c == "A" or c =="a":
+        print "X",
+    else:
+        print c,
+
+#Don't delete this print statement!
+print
+"""for some reason this extraneous print statement is needed to
+make this code work."""
